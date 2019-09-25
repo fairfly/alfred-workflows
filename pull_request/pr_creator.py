45b2111374eeb7722458abf80eb7eb20114d2570
noam <noam@fairfly.com>
2019-09-25 10:10:04 +0300

    Take the github access token from env

--- pull_request/pr_creator.py
+++ pull_request/pr_creator.py
@@ -14,7 +14,9 @@ class FFGitHub:
     FF_TASK_PATTERN = r'(FF[\s*\-\s*]*)(?P<task>\d*)(?P<desc>.*)'
 
     def __init__(self, repo_name):
-        self.github = Github(login_or_token='671afe0d3294969967a9eecb4398b3d1eaad8214')
+	github_token=os.environ.get('GITHUB_ACCESS_TOKEN')
+	print(github_token)
+        self.github = Github(login_or_token=github_token)
         self.main_repo = self.get_repo(repo_name)
 
     def get_repo(self, repo_name):
